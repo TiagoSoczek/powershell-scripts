43d9d0148fd29016d15ff8476ca6fc4ae10fa7ff
Tiago Soczek <tiagosoczek@gmail.com>
2016-12-17 13:30:15 -0200

    Instruções para execução

--- azure-remove-all-resources.ps1
+++ azure-remove-all-resources.ps1
@@ -1,6 +1,6 @@
 # WARNING: This will remove ALL resources groups of current subscription
-# Other commands
-# Get-AzureRmSubscription
-# Select-AzureRmSubscription
-
+# 1) Realizar login pelo comando: Login-AzureRmAccount
+# 2) Visualizar subscriptions pelo comando: Get-AzureRmSubscription 
+# 3) Selecionar subscription default. Select-AzureRmSubscription -SubscriptionName "Nome da subscription"
+# 4) Executar comando abaixo:
 Get-AzureRmResourceGroup | % { Remove-AzureRmResourceGroup -Name $_.ResourceGroupName -Verbose -Force; }
